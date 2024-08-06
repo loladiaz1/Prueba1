08296fd6ea52b160825416b61e1a53872e4ac56f
ccsalemme <csalemme@uade.edu.ar>
2024-08-06 21:53:25 +0000

    commit4

---
+++ prueba11/conjuntoTDA.java
@@ -0,0 +1,10 @@
+package prueba11;
+
+public interface conjuntoTDA {
+    void InicializarConjunto();
+    void Agregar(int x);
+    void Sacar(int x);
+    int Elegir();
+    boolean Pertenece(int x);
+    boolean ConjuntoVacio();
+}
